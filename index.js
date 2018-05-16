5f29f73e9d86a1b929cc1048cfd9f1b42e7525d7
briecodes <hello@brie.work>
2018-05-16 14:00:16 -0400

    lab completed.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file
+function findMatching(drivers, string){
+    const newArray = []
+    for (const name of drivers){
+        if (name.toLowerCase() === string.toLowerCase()){
+            newArray.push(name);
+        }
+    }
+    return newArray;
+}
+
+function fuzzyMatch(drivers, string){
+    const num = string.length;
+    const newArray = [];
+    for (const name of drivers){
+        if (name.substring(0,num) === string){
+            newArray.push(name);
+        }
+    }
+    return newArray;
+}
+
+function matchName(drivers, string){
+    const newArray = [];
+    for (const person of drivers){
+        if (person["name"] === string ){
+            newArray.push(person);
+        }
+    }
+    return newArray;
+}
